c5fee0f05200a88efa5bcafa6db888b41782afe8
JerryWong711 <minqiao@gmail.com>
2021-12-09 16:57:13 +0800

    fix docker-compose.yaml

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,9 @@
 export PYTHONPATH=/Savior/
 if [ -f /Savior/docker/firstrun ]; then
     . /Savior/.env
-    echo -e "$DB_HOST\tmysql" >> /etc/hosts
+    if [ "$DB_HOST" != "mysql" ]; then
+        echo -e "$DB_HOST\tmysql" >> /etc/hosts
+    fi
     /Savior/docker/wait-for-it.sh -t 0 mysql:3306
     python3 /Savior/manage.py makemigrations api
     python3 /Savior/manage.py migrate
